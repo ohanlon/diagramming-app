39f252c0aaa73e722dff43af7e485a5873620489
ohanlon <pete.ohanlon@gmail.com>
2025-09-08 11:28:11 +0100

    Fix: Update jest to overcome issues running tests
    The Jest capability needed to be updated to allow import entries being added into the contents.

--- diagramming-app/jest.config.cjs
+++ diagramming-app/jest.config.cjs
@@ -4,9 +4,15 @@ module.exports = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
-    '\\.(less|css)$': 'identity-obj-proxy',
-    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/src/__mocks__/fileMock.js',
+    '\.(less|css)$': 'identity-obj-proxy',
+    '\.(gif|ttf|eot|svg|png)$': '<rootDir>/src/__mocks__/fileMock.js',
   },
+  transform: {
+    '^.+\.[tj]sx?$': 'ts-jest',
+  },
+  transformIgnorePatterns: [
+    '/node_modules/(?!color-convert)/',
+  ],
   testMatch: ['<rootDir>/src/**/*.test.tsx'],
   globals: {
     'ts-jest': {
