4646f86f7296358a99da1bc1970d1d6b1bfabaac
ohanlon <pete.ohanlon@gmail.com>
2025-09-15 14:36:00 +0100

    Enhance: Update the main toolbar.

--- diagramming-app/src/components/MainToolbar/MainToolbar.tsx
+++ diagramming-app/src/components/MainToolbar/MainToolbar.tsx
@@ -1,46 +1,68 @@
 import React, { useState } from 'react';
-import { AppBar, Toolbar, Button, Menu, MenuItem } from '@mui/material';
+import { Toolbar, Button, Menu, MenuItem } from '@mui/material';
+import { ArrowRight } from '@mui/icons-material';
 import { useDiagramStore } from '../../store/useDiagramStore';
 
 const MainToolbar: React.FC = () => {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [newMenuAnchorEl, setNewMenuAnchorEl] = useState<null | HTMLElement>(null);
   const { resetStore } = useDiagramStore();
 
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+  const handleMenuOpen = (event: React.MouseEvent<HTMLButtonElement>) => {
     setAnchorEl(event.currentTarget);
   };
 
-  const handleClose = () => {
+  const handleMenuClose = () => {
     setAnchorEl(null);
+    setNewMenuAnchorEl(null);
   };
 
-  const handleNewDocument = () => {
+  const handleNewSubMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setNewMenuAnchorEl(event.currentTarget);
+  };
+
+  const handleNewSubMenuClose = () => {
+    setNewMenuAnchorEl(null);
+  };
+
+  const handleNewDiagram = () => {
     resetStore();
-    handleClose();
+    handleMenuClose();
   };
 
   return (
     <Toolbar disableGutters variant="dense" sx={{ borderBottom: '1px solid #e0e0e0', padding: '0 0', marginLeft: 0, boxShadow: 'none', color: 'black' }}>
-      <Button
-        id="file-button"
-        aria-controls={anchorEl ? 'file-menu' : undefined}
-        aria-haspopup="true"
-        aria-expanded={anchorEl ? 'true' : undefined}
-        onClick={handleClick}
-        sx={{ color: 'black' }}
-      >
+      <Button onClick={handleMenuOpen} sx={{ color: 'black' }}>
         File
       </Button>
       <Menu
-        id="file-menu"
         anchorEl={anchorEl}
         open={Boolean(anchorEl)}
-        onClose={handleClose}
-        MenuListProps={{
-          'aria-labelledby': 'file-button',
+        onClose={handleMenuClose}
+      >
+        <MenuItem
+          onMouseOver={handleNewSubMenuOpen}
+          sx={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}
+        >
+          New
+          <ArrowRight sx={{ fontSize: '1.2rem', ml: 1 }} />
+        </MenuItem>
+      </Menu>
+      <Menu
+        anchorEl={newMenuAnchorEl}
+        open={Boolean(newMenuAnchorEl)}
+        onClose={handleNewSubMenuClose}
+        MenuListProps={{ onMouseLeave: handleNewSubMenuClose }}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
         }}
       >
-        <MenuItem onClick={handleNewDocument}>New document</MenuItem>
+        <MenuItem onClick={handleNewDiagram}>Diagram</MenuItem>
       </Menu>
     </Toolbar>
   );
